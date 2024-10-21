8ddadf065c2ede056ec4672219a2eb9d207b061a
Fahad.Hassan <fahadfuuast2@gmail.com>
2024-10-21 22:31:11 +0500

    dlt btn added

--- todo-app-thapa/src/Todo/Todo.jsx
+++ todo-app-thapa/src/Todo/Todo.jsx
@@ -1,41 +1,50 @@
 import React, { useState } from "react";
-import { MdCheck,MdDeleteForever } from     "react-icons/md";
-import './Todo.css'
+import { MdCheck, MdDeleteForever } from "react-icons/md";
+import "./Todo.css";
 export const Todo = () => {
-    const [inputValue, setInputValue] = useState('');
-    const [task,setTask] = useState([])
-    const [dateTime, setDateTime] = useState()
-    const handleInputChange = (value) => {
-        setInputValue(value)
+  const [inputValue, setInputValue] = useState("");
+  const [task, setTask] = useState([]);
+  const [dateTime, setDateTime] = useState();
+  const handleInputChange = (value) => {
+    setInputValue(value);
+  };
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (!inputValue) return;
+
+    setTask((prevTask) => [...prevTask, inputValue]);
+
+    if (task.includes(inputValue)) {
+      return;
     }
-    const handleFormSubmit = (e) => {
-        e.preventDefault()
+    setInputValue("");
+  };
 
-        if(!inputValue) return
+  // todo date time
+  setInterval(() => {
+    const now = new Date();
+    const formattedDate = now.toLocaleDateString();
+    const formattedTime = now.toLocaleTimeString();
+    setDateTime(`${formattedDate} ${formattedTime}`);
+  }, 1000);
 
-        setTask((prevTask) => [...prevTask, inputValue])
+  // todo handleDltTodo function
 
-        if(task.includes(inputValue)){
-            return
-        }
-        setInputValue("")
-    }
-    
-    // todo date time
-    setInterval(()=>{
-        const now = new Date()
-        const formattedDate = now.toLocaleDateString()
-        const formattedTime = now.toLocaleTimeString()
-        setDateTime(`${formattedDate} ${formattedTime}`)
-    },1000)
+  const handleDltTodo = (curTaskVal) => {
+    // console.log(task);
+    // console.log(curTaskVal);
+    const updatedTask = task.filter((curTask) => curTask !== curTaskVal)
+    setTask(updatedTask)
+  }
 
 
-    return (
+  return (
     <>
       <section className="todo-container">
         <header>
           <h1>Todo List</h1>
-           <h3 className="date-time">{dateTime}</h3>
+          <h3 className="date-time">{dateTime}</h3>
         </header>
         <section className="form" onSubmit={handleFormSubmit}>
           <form>
@@ -49,24 +58,31 @@ export const Todo = () => {
               />
             </div>
             <div>
-              <button type="submit" className="todo-btn">Add Task</button>
+              <button type="submit" className="todo-btn">
+                Add Task
+              </button>
             </div>
           </form>
         </section>
         <section className="myUnOrdList">
-            <ul>
-                {
-                    task.map((curTask,i) => {
-                        return(
-                            <li key={i} className="todo-item">
-                                <span>{curTask}</span>
-                                <button className="check-btn"><MdCheck /></button>
-                                <button className="delete-btn"><MdDeleteForever /></button>
-                            </li>
-                        )
-                    })
-                }
-            </ul>
+          <ul>
+            {task.map((curTask, i) => {
+              return (
+                <li key={i} className="todo-item">
+                  <span>{curTask}</span>
+                  <button className="check-btn">
+                    <MdCheck />
+                  </button>
+                  <button
+                    className="delete-btn"
+                    onClick={() => handleDltTodo(curTask)}
+                  >
+                    <MdDeleteForever />
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
         </section>
       </section>
     </>
